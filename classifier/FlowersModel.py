234339e42e922e5dc43e75b71716c192f3f330bf
Oliver Dixon <od641@york.ac.uk>
2024-05-06 01:42:59 +0100

    Isolate the model in FlowersModel.py

--- classifier/FlowersModel.py
+++ classifier/FlowersModel.py
@@ -1,62 +1,37 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
-from typing import Final
 
 
-class FlowersModel(torch.nn.Module):
-    """ Encapsulates a convolutional neural network model for the `Flowers-102` dataset from Oxford University; see
-    https://www.robots.ox.ac.uk/~vgg/data/flowers/102/.
-    """
-    __BATCH_SIZE: Final[int] = 64
-    __DATASET_TEMPLATES: Final[dict[str, transforms.Compose]] = {
-        "train": transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
-        ]),
+class FlowersModel(nn.Module):
+    def forward(self, data):
+        data = self._pool(nn.functional.relu(self._bn1(self._conv1(data))))
+        data = self._pool(nn.functional.relu(self._bn2(self._conv2(data))))
+        data = self._pool(nn.functional.relu(self._bn3(self._conv3(data))))
 
-        "test": transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
-        ]),
+        data = data.view(-1, 128 * 26 * 26)
 
-        "val": transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor()
-        ])
-    }
+        data = nn.functional.relu(self._lin1(data))
+        data = nn.functional.relu(self._lin2(data))
+        data = nn.functional.relu(self._lin3(data))
 
-    @staticmethod
-    def __download_data_split(split: str) -> DataLoader:
-        return DataLoader(
-            datasets.Flowers102(
-                root="data",
-                split=split,
-                download=True,
-                transform=FlowersModel.__DATASET_TEMPLATES[split]
-            ),
+        return self._lin4(data)
 
-            batch_size=FlowersModel.__BATCH_SIZE
-        )
+    def __init__(self):
+        super(FlowersModel, self).__init__()
 
-    def __prepare_data(self) -> None:
-        self.__training_data = self.__download_data_split("train")
-        self.__test_data = self.__download_data_split("test")
-        self.__validation_data = self.__download_data_split("val")
+        self._bn1 = nn.BatchNorm2d(32)
+        self._bn2 = nn.BatchNorm2d(64)
+        self._bn3 = nn.BatchNorm2d(128)
 
-    def __init__(self, in_features, hidden_layers, out_features, activation=nn.functional.relu):
-        super().__init__()
-        self.__prepare_data()
+        self._conv1 = nn.Conv2d(3, 32, 3)
+        self._conv2 = nn.Conv2d(32, 64, 3)
+        self._conv3 = nn.Conv2d(64, 128, 3)
 
+        self._pool = nn.MaxPool2d(2, 2)
 
-if __name__ == '__main__':
-    if torch.cuda.is_available():
-        torch.set_default_device("cuda:0")
-    print(f"{torch.cuda.device_count()} CUDA GPUs are available; using {torch.get_default_device()}")
+        self._lin1 = nn.Linear(128 * 26 * 26, 2048)
+        self._lin2 = nn.Linear(2048, 1024)
+        self._lin3 = nn.Linear(1024, 512)
+        self._lin4 = nn.Linear(512, 102)
 
-    classifier = FlowersModel(None, None, None)
+        self._optimiser = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.9)
